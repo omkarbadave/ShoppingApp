c0fc01b8a29bb3e0e5e5543236e3c5a78ffdad69
omkarb2 <omkarb2@kpit.com>
2021-02-01 16:25:53 +0530

    Implemented Redux for sign and sign out.

--- src/App.js
+++ src/App.js
@@ -9,32 +9,26 @@ import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.util";
 
-class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			currentUser: null,
-		};
-	}
+import { connect } from "react-redux";
+import { setCurrentUser } from "./redux/user/user.actions";
 
+class App extends React.Component {
 	userUnsubscribeFromAuth = null;
 
 	componentDidMount() {
+		const { setCUser } = this.props;
 		this.userUnsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
 			if (userAuth) {
 				const userRef = await createUserProfileDocument(userAuth);
 				userRef.onSnapshot((snapShot) => {
-					this.setState({
-						currentUser: {
-							id: snapShot.id,
-							...snapShot.data(),
-						},
+					setCUser({
+						id: snapShot.id,
+						...snapShot.data(),
 					});
-					console.log(this.state);
 				});
 			}
 
-			this.setState({ currentUser: userAuth });
+			setCUser(userAuth);
 		});
 	}
 
@@ -46,7 +40,7 @@ class App extends React.Component {
 	render() {
 		return (
 			<div>
-				<Header currentUser={this.state.currentUser} />
+				<Header />
 				<Switch>
 					<Route exact path="/" component={HomePage} />
 					<Route path="/shop" component={ShopPage} />
@@ -57,4 +51,8 @@ class App extends React.Component {
 	}
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => ({
+	setCUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+import { createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+
+import rootReducer from "./root-reducer";
+
+const middlerware = [logger];
+
+const store = createStore(rootReducer, applyMiddleware(...middlerware));
+
+export default store;
