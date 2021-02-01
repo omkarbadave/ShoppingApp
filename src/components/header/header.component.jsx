0a2e2fd8d8aa16381ca1f4eae9fec54800044438
omkarb2 <omkarb2@kpit.com>
2021-02-01 19:12:55 +0530

    Added cart-icon and  dropdown to header

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,6 +6,8 @@ import { ReactComponent as Logo } from "../../assets/crown.svg";
 import { auth } from "../../firebase/firebase.util";
 
 import "./header.styles.scss";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdow.component";
 
 const Header = ({ currentUser }) => (
 	<div className="header">
@@ -28,7 +30,9 @@ const Header = ({ currentUser }) => (
 					SIGN IN
 				</Link>
 			)}
+			<CartIcon />
 		</div>
+		<CartDropdown />
 	</div>
 );
 
