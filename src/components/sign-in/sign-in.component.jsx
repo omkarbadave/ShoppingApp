2161654167a98dde2d39b05fe969ca1c9e0a6edf
omkarb2 <omkarb2@kpit.com>
2021-02-01 21:20:14 +0530

    Fixed issue in login and form validation

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -7,67 +7,71 @@ import "./sign-in.styles.scss";
 import { auth, signInWithGoogle } from "../../firebase/firebase.util";
 
 class SignIn extends React.Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      email: "",
-      password: "",
-    };
-  }
+		this.state = {
+			email: "",
+			password: "",
+		};
+	}
 
-  handleSubmit = async (event) => {
-    event.preventDefault();
+	handleSubmit = async (event) => {
+		event.preventDefault();
 
-    const { email, password } = this.state;
+		const { email, password } = this.state;
 
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: "", password: "" });
-    } catch (error) {
-      console.log(error.message);
-    }
-  };
+		try {
+			await auth.signInWithEmailAndPassword(email, password);
+			this.setState({ email: "", password: "" });
+		} catch (error) {
+			console.log(error.message);
+		}
+	};
 
-  handleChange = (event) => {
-    const { value, name } = event.target;
+	handleChange = (event) => {
+		const { value, name } = event.target;
 
-    this.setState({ [name]: value });
-  };
+		this.setState({ [name]: value });
+	};
 
-  render() {
-    return (
-      <div className='sign-in'>
-        <h2>I already have an account</h2>
-        <span>Sign in with your email and password</span>
+	render() {
+		return (
+			<div className="sign-in">
+				<h2>I already have an account</h2>
+				<span>Sign in with your email and password</span>
 
-        <form onSubmit={this.handleSubmit}>
-          <FormInput
-            name='email'
-            type='email'
-            handleChange={this.handleChange}
-            value={this.state.email}
-            label='Email'
-            required
-          />
-          <FormInput
-            name='password'
-            type='password'
-            value={this.state.password}
-            handleChange={this.handleChange}
-            label='Passowrd'
-            required
-          />
-          <div className='buttons'>
-            <CustomButton type='submit'> Sign in </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
-              SignIn With Google
-            </CustomButton>
-          </div>
-        </form>
-      </div>
-    );
-  }
+				<form onSubmit={this.handleSubmit}>
+					<FormInput
+						name="email"
+						type="email"
+						handleChange={this.handleChange}
+						value={this.state.email}
+						label="Email"
+						required
+					/>
+					<FormInput
+						name="password"
+						type="password"
+						value={this.state.password}
+						handleChange={this.handleChange}
+						label="Passowrd"
+						required
+					/>
+					<div className="buttons">
+						<CustomButton type="submit"> Sign in </CustomButton>
+						<CustomButton
+							type="button"
+							onClick={signInWithGoogle}
+							isGoogleSignIn
+						>
+							SignIn With Google
+						</CustomButton>
+					</div>
+				</form>
+			</div>
+		);
+	}
 }
 
 export default SignIn;
--- src/redux/user/user-reducer.jsx
+++ src/redux/user/user-reducer.jsx
@@ -6,7 +6,7 @@ const INITIAL_STATE = {
 
 const userReducer = (state = INITIAL_STATE, action) => {
 	switch (action.type) {
-		case userActionTypes.INITIAL_STATE:
+		case userActionTypes.SET_CURRENT_USER:
 			return {
 				...state,
 				currentUser: action.payload,
