faeb0e18d5b9021ca463936b05ea00ee456815b3
omkarb2 <omkarb2@kpit.com>
2021-02-03 01:15:46 +0530

    Added code to display total cart item count in cart icon

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -6,10 +6,10 @@ import "./cart-icon.styles.scss";
 
 import { ReactComponent as ShoppingIcon } from "../../assets/shopping-bag.svg";
 
-const CartIcon = ({ toggleCartHiddenProp }) => (
+const CartIcon = ({ toggleCartHiddenProp, itemCount }) => (
 	<div className="cart-icon" onClick={toggleCartHiddenProp}>
 		<ShoppingIcon className="shopping-icon" />
-		<span className="item-count">0</span>
+		<span className="item-count">{itemCount}</span>
 	</div>
 );
 
@@ -17,4 +17,10 @@ const mapDispatchToProps = (dispatch) => ({
 	toggleCartHiddenProp: () => dispatch(toggleCartHidden()),
 });
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+	itemCount: cartItems.reduce((totalCount, cartItem) => {
+		return totalCount + cartItem.quantity;
+	}, 0),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
